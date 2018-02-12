edfae7ce986aebebba1ef682d001bf34cdf2ac1e
ASVS-Kartheek <f20160015@hyderabad.bits-pilani.ac.in>
2018-02-12 16:28:47 +0530

    Create Maps Activity

---
+++ app/src/main/java/com/dota/pearl18/pearlapp2018/Articles/MapsActivity.java
@@ -0,0 +1,13 @@
+package com.dota.pearl18.pearlapp2018.Articles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+    }
+}
