0a4feca6fc8db9f1bfb15f2cc166350448c3bcb7
Harshvardhan Takawale <harshvardhantakawale@gmail.com>
2018-02-12 18:30:03 +0530

    places api mock, testing reqd

--- app/src/main/java/com/dota/pearl18/pearlapp2018/Articles/MainActivity.java
+++ app/src/main/java/com/dota/pearl18/pearlapp2018/Articles/MainActivity.java
@@ -1,22 +1,68 @@
 package com.dota.pearl18.pearlapp2018.Articles;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import com.dota.pearl18.pearlapp2018.R;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MyActivity";
+    private Button TreasureHuntButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TreasureHuntButton = findViewById(R.id.treasure);
+
+        TreasureHuntButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                PlaceDetectionClient mPlaceDetectionClient;
+
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
+                    Task<PlaceLikelihoodBufferResponse> placeResult = mPlaceDetectionClient.getCurrentPlace(null);
+                    placeResult.addOnCompleteListener(new OnCompleteListener<PlaceLikelihoodBufferResponse>() {
+                        @Override
+                        public void onComplete(@NonNull Task<PlaceLikelihoodBufferResponse> task) {
+                            PlaceLikelihoodBufferResponse likelyPlaces = task.getResult();
+                            for (PlaceLikelihood placeLikelihood : likelyPlaces) {
+                                Log.i(TAG, String.format("Place '%s' has likelihood: %g",
+                                        placeLikelihood.getPlace().getName(),
+                                        placeLikelihood.getLikelihood()));
+                            }
+                            likelyPlaces.release();
+                        }
+                    });
+
+                    return;
+                }
+            }
+        });
+
 
 
 
