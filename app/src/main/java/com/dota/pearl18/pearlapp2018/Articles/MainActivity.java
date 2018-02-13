f4f55f001a5b26966e6b0f17f08029851d8ee941
ASVS-Kartheek <f20160015@hyderabad.bits-pilani.ac.in>
2018-02-13 18:32:17 +0530

    Complete Maps Activity

--- app/src/main/java/com/dota/pearl18/pearlapp2018/Articles/MainActivity.java
+++ app/src/main/java/com/dota/pearl18/pearlapp2018/Articles/MainActivity.java
@@ -1,5 +1,6 @@
 package com.dota.pearl18.pearlapp2018.Articles;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,17 +9,26 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import com.dota.pearl18.pearlapp2018.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mGuideBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        mGuideBtn = findViewById(R.id.main_guide_btn);
+        mGuideBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MapsActivity.class));
+            }
+        });
 
     }
 
