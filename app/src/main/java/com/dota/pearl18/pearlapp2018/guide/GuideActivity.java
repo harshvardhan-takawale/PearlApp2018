b150cc55dfb647b509d4e64eba1bb9b50e8bc14e
Harshvardhan Takawale <harshvardhantakawale@gmail.com>
2018-02-21 00:59:20 +0530

    animation mockup

--- app/src/main/java/com/dota/pearl18/pearlapp2018/guide/GuideActivity.java
+++ app/src/main/java/com/dota/pearl18/pearlapp2018/guide/GuideActivity.java
@@ -1,6 +1,7 @@
 package com.dota.pearl18.pearlapp2018.guide;
 
 import android.Manifest;
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -10,7 +11,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.dota.pearl18.pearlapp2018.Articles.TextDisplayActivity;
@@ -46,6 +49,25 @@ public class GuideActivity extends AppCompatActivity {
                 animateText(view,"dir");
             }
         });
+
+        final ImageView backgroundOne = (ImageView) findViewById(R.id.background_one);
+        final ImageView backgroundTwo = (ImageView) findViewById(R.id.background_two);
+
+        final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setDuration(10000L);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                final float progress = (float) animation.getAnimatedValue();
+                final float width = backgroundOne.getWidth();
+                final float translationX = width * progress;
+                backgroundOne.setTranslationX(translationX);
+                backgroundTwo.setTranslationX(translationX - width);
+            }
+        });
+        animator.start();
     }
 
     @Override
