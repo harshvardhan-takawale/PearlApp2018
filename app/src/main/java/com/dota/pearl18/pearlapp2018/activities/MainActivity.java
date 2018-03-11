4a31a0e67691b0a1117a8aedbd89269264258e8d
C Shri Akhil <shriakhilc@gmail.com>
2018-03-11 14:01:21 +0530

    Shift animations from Guide to Landing

--- app/src/main/java/com/dota/pearl18/pearlapp2018/activities/MainActivity.java
+++ app/src/main/java/com/dota/pearl18/pearlapp2018/activities/MainActivity.java
@@ -1,12 +1,16 @@
 package com.dota.pearl18.pearlapp2018.activities;
 
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.dota.pearl18.pearlapp2018.Adapters.LandingAdapter;
@@ -95,33 +99,85 @@ public class MainActivity extends AppCompatActivity {
             .build());
         onItemChanged(0);
 
-        /*  LANDING ANIMATION CODE
-        // backgroundOne is the initially visible image. It will then be scrolled off the screen
-        // toward the left, and the other two will scroll in. This goes on till the third image
-        // i.e. backgroundThree perfectly fits the screen. After which we switch to backgroundOne.
-        final ImageView backgroundOne = findViewById(R.id.land_left_one);
-        final ImageView backgroundTwo = findViewById(R.id.land_left_one);
-        final ImageView backgroundThree = findViewById(R.id.land_right_two);
+        // landRightOne and cloudRightOne are the initially visible layers.
+        // They will then be scrolled off the screen toward the left, and the other two will scroll in.
+        // This goes on till the third image perfectly fits the screen.
+        // After which we switch back to the RightOne(s).
+        final ImageView landRightOne = findViewById(R.id.land_right_one);
+        final ImageView cloudRightOne = findViewById(R.id.cloud_right_one);
 
-        final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
-        animator.setRepeatCount(ValueAnimator.INFINITE);
-        animator.setInterpolator(new LinearInterpolator());
-        animator.setDuration(10000L);
-        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+        final ImageView landLeftOne = findViewById(R.id.land_left_one);
+        final ImageView cloudLeftOne = findViewById(R.id.cloud_left_one);
+
+        final ImageView landRightTwo = findViewById(R.id.land_right_two);
+        final ImageView cloudRightTwo = findViewById(R.id.cloud_right_two);
+
+        final ImageView busFull = findViewById(R.id.layer_bus);
+
+        ValueAnimator landAnimator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        landAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        landAnimator.setInterpolator(new LinearInterpolator());
+        landAnimator.setDuration(10000L);
+        landAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
                 final float progress = (float) animation.getAnimatedValue();
-                final float width = backgroundOne.getWidth();
+                final float width = landRightOne.getWidth();
                 // determine how much to the left should the image be shifted.
                 // when the animation terminates, backgroundOne should be at (-2 * width)
                 final float translationX = width * (progress * 2.0f); // this is a positive value
-                backgroundOne.setTranslationX(0 - translationX);
-                backgroundTwo.setTranslationX(width - translationX);
-                backgroundThree.setTranslationX(2 * width - translationX);
+
+                landRightOne.setTranslationX(0 - translationX);
+
+                landLeftOne.setTranslationX(width - translationX);
+
+                landRightTwo.setTranslationX(2 * width - translationX);
             }
         });
-        animator.start();
-        */
+
+        ValueAnimator cloudAnimator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        cloudAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        cloudAnimator.setInterpolator(new LinearInterpolator());
+        cloudAnimator.setDuration(30000L);
+        cloudAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                final float progress = (float) animation.getAnimatedValue();
+                final float width = cloudRightOne.getWidth();
+                // determine how much to the left should the image be shifted.
+                // when the animation terminates, backgroundOne should be at (-2 * width)
+                final float translationX = width * (progress * 2.0f); // this is a positive value
+
+                cloudRightOne.setTranslationX(0 - translationX);
+
+                cloudLeftOne.setTranslationX(width - translationX);
+
+                cloudRightTwo.setTranslationX(2 * width - translationX);
+            }
+        });
+
+        ValueAnimator busAnimator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        busAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        busAnimator.setInterpolator(new LinearInterpolator());
+        busAnimator.setDuration(1000L);
+        busAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                final float progress = (float) animation.getAnimatedValue();
+                final float displacement = 0.01f * busFull.getHeight(); // 0.5% of the total height
+                if (progress < 0.5f) {
+                    // move down for first half
+                    busFull.setTranslationY(2.0f * progress * displacement);
+                } else {
+                    // move back up for second half
+                    busFull.setTranslationY(2.0f * (1.0f - progress)* displacement);
+                }
+            }
+        });
+
+        landAnimator.start();
+        cloudAnimator.start();
+        busAnimator.start();
 
     }
 
@@ -129,5 +185,4 @@ public class MainActivity extends AppCompatActivity {
         name = findViewById(R.id.Button_name);
         name.setText(buttonList.get(pos).getButtonName());
     }
-
 }
