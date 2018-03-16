443bdc8cdbd9e21cef558f489df1cf196da6514f
C Shri Akhil <shriakhilc@gmail.com>
2018-03-17 01:02:57 +0530

    Add items to credits activity

--- app/src/main/java/com/dota/pearl18/pearlapp2018/activities/CreditsActivity.java
+++ app/src/main/java/com/dota/pearl18/pearlapp2018/activities/CreditsActivity.java
@@ -45,15 +45,14 @@ public class CreditsActivity extends AppCompatActivity {
         details.add(new DeveloperDetails("C Shri Akhil", R.drawable.dev_akhil, new String[] {"https://www.facebook.com/c.akhil.shri","https://github.com/TheGamer007/","https://www.linkedin.com/in/shriakhilc/",null}));
         details.add(new DeveloperDetails("Harshvardhan Takawale", R.drawable.dev_harshvardhan, new String[] { "https://www.facebook.com/harshvardhan.takawale","https://github.com/harshvardhan-takawale","https://www.linkedin.com/in/harshvardhan-takawale-9b5125142/",null}));
         details.add(new DeveloperDetails("Ashwik Reddy", R.drawable.dev_ashwik, new String[] {"https://www.facebook.com/ashwik.aileni","https://github.com/Ashwik",null,null}));
-
         return details;
     }
 
     private ArrayList<DeveloperDetails> getAndroidDevs2(){
         ArrayList<DeveloperDetails> details = new ArrayList<>();
-        details.add(new DeveloperDetails("Kartheek Akella", R.drawable.dev_kartheek, new String[]{"","","",null} ));
-        details.add(new DeveloperDetails("Prateek Agarwal", R.drawable.dev_prateek, new String[]{"","","",null}));
-        details.add(new DeveloperDetails("Vineeth P", R.drawable.dev_vineeth, new String[]{"","","",null}));
+        details.add(new DeveloperDetails("Kartheek Akella", R.drawable.dev_kartheek, new String[]{"https://www.facebook.com/kartheek.asvs","https://github.com/ASVS-Kartheek",null,null} ));
+        details.add(new DeveloperDetails("Prateek Agarwal", R.drawable.dev_prateek, new String[]{"https://www.facebook.com/prateek.agarwal.94801","https://github.com/prat-bphc52",null,null}));
+        details.add(new DeveloperDetails("Vineeth P", R.drawable.dev_vineeth, new String[]{"https://www.facebook.com/vineeth.vinny.007","https://github.com/vineeth07",null,null}));
         return details;
     }
 
@@ -61,16 +60,16 @@ public class CreditsActivity extends AppCompatActivity {
         ArrayList<DeveloperDetails> details = new ArrayList<>();
 
         details.add(new DeveloperDetails("Rohitt Vashishtha", R.drawable.dev_rohitt, new String[] {"https://www.facebook.com/VagrantRohitt","https://github.com/aero31aero",null,null}));
-        details.add(new DeveloperDetails("Sohail Rajdev", R.drawable.profile_icon, new String[]{"","","", null}));
+        details.add(new DeveloperDetails("Sohail Rajdev", R.drawable.profile_icon, new String[]{"https://www.facebook.com/srajdev97","",null, null}));
         return details;
     }
 
     private ArrayList<DeveloperDetails> getDesigners(){
         ArrayList<DeveloperDetails> details = new ArrayList<>();
 
-        details.add(new DeveloperDetails("Suraj Thotakura", R.drawable.profile_icon, new String[]{"", null, null, ""}));
+        details.add(new DeveloperDetails("Suraj Thotakura", R.drawable.profile_icon, new String[]{"https://www.facebook.com/satya.suraj.7", null, null, ""}));
         details.add(new DeveloperDetails("Govind Savio", R.drawable.profile_icon, new String[]{"", null, null, ""}));
-        details.add(new DeveloperDetails("Abishek Yadav", R.drawable.profile_icon, new String[]{"",null,null,""}));
+        details.add(new DeveloperDetails("Abishek Yadav", R.drawable.profile_icon, new String[]{"https://www.facebook.com/profile.php?id=100000513640766",null,null,""}));
         return details;
     }
 
